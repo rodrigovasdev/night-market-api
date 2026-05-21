31d5cfad3126f2797989dd45e6ed784d90df81b9
rodrigovasdev <146775218+rodrigovasdev@users.noreply.github.com>
2026-05-20 21:22:48 -0400

    fix: remove trailing slash from CORS origin URL

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
     app.enableCors({
     origin: ['http://localhost:3001',
-    'https://night-market-black.vercel.app/',],
+    'https://night-market-black.vercel.app',],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
